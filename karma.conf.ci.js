c5409f732f78ba7ba91a29a7028ea5d237757235
taron <taron@SPACE-STATION>
2023-09-02 01:07:10 -0400

    another karma config

--- karma.conf.ci.js
+++ karma.conf.ci.js
@@ -2,49 +2,49 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 
-// process.env.CHROME_BIN = require('puppeteer').executablePath()
+process.env.CHROME_BIN = require('puppeteer').executablePath()
 
-// module.exports = function (config) {
+module.exports = function (config) {
 
-//   config.set({
-//     basePath: '',
-//     frameworks: ['jasmine', '@angular-devkit/build-angular'],
-//     plugins: [
-//       require('karma-jasmine'),
-//       require('karma-jasmine-html-reporter'),
-//       require('karma-coverage'),
-//       require('karma-junit-reporter'),
-//       require('@angular-devkit/build-angular/plugins/karma'),
-//       require('karma-chrome-launcher'),
-//     ],
-//     client: {
-//       jasmine: {
-//         // you can add configuration options for Jasmine here
-//         // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-//         // for example, you can disable the random execution with `random: false`
-//         // or set a specific seed with `seed: 4321`
-//       },
-//       clearContext: false // leave Jasmine Spec Runner output visible in browser
-//     },
-//     jasmineHtmlReporter: {
-//       suppressAll: true // removes the duplicated traces
-//     },
-//     coverageReporter: {
-//       dir: require('path').join(__dirname, './coverage/truthfulls-ui'),
-//       subdir: '.',
-//       reporters: [
-//         { type: 'html' },
-//         { type: 'text-summary' }
-//       ]
-//     },
-//     reporters: ['progress', 'kjhtml', 'junit'],
-//     browsers: ['ChromeHeadless'],
-//     plugins: [
-//       'karma-chrome-launcher',
-//       'karma-junit-reporter'
-//   ],
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('karma-junit-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-chrome-launcher'),
+    ],
+    client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/truthfulls-ui'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml', 'junit'],
+    browsers: ['ChromeHeadless'],
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-junit-reporter'
+  ],
 
-//     singleRun: true,
-//     restartOnFileChange: false
-//   });
-// };
+    singleRun: true,
+    restartOnFileChange: false
+  });
+};
