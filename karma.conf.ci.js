db22dad801ab3de810874b8f20ac39220cad1a02
taron <taron@SPACE-STATION>
2023-09-01 22:16:38 -0400

    added chrome headless to karma.conf

--- karma.conf.ci.js
+++ karma.conf.ci.js
@@ -37,7 +37,7 @@ module.exports = function (config) {
       ]
     },
     reporters: ['progress', 'kjhtml', 'junit'],
-    browsers: ['PhantomJS'],
+    browsers: ['ChromeHeadless'],
     singleRun: true,
     restartOnFileChange: false
   });
