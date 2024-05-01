f7b3729d9ff051979ee5eb484bd2e88a10954f3d
taron <taron@SPACE-STATION>
2024-04-30 23:55:45 -0400

    fixed tests

--- src/app/plot/plot.component.spec.ts
+++ src/app/plot/plot.component.spec.ts
@@ -1,14 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { PlotComponent } from './plot.component';
 
-describe('PlotComponentComponent', () => {
+describe('PlotComponent', () => {
   let component: PlotComponent;
   let fixture: ComponentFixture<PlotComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PlotComponent]
+      imports: [PlotComponent,BrowserAnimationsModule,HttpClientTestingModule],  
     })
     .compileComponents();
     
